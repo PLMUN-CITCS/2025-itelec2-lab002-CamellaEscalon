6b7a46cbecdc195b44f296a9787c0811babab8af
CamellaEscalon <>
2025-03-10 12:54:22 +0800

    Last Sync: 2025-03-10 04:54:22 (Mobile)

--- problem_01.py
+++ problem_01.py
@@ -1,10 +1,13 @@
-# YOUR NAME
-# ITELEC2
-# Problem Set 01 - Problem 01
-# Simple Calculator Program
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
 
-def main():
-    pass # replace this line with all of your code
 
-if __name__ == "__main__":
-    main()
+sum_result = num1 + num2
+diff_result = num1 - num2
+prod_result = num1 * num2
+
+
+print(f"The sum is {num1 + num2}")
+print(f"The difference is {num1 - num2}")
+print(f"The product is {num1 * num2}")
+print(f"The quotient is {(num1 / num2):.2f}
